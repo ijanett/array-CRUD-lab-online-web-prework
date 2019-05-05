e8cad2bdc1b3e0c893041b50021a1e363844b05e
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-05 20:47:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "Lauren Daigle"
+  create_an_array.push "Lauren Daigle"
 end
 
 def add_element_to_start_of_array(array, element)
